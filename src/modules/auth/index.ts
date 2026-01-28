5f9385a62e196697aab0035a9ff8071efab5e2ed
Julio Macedo <juliocesar1076@gmail.com>
2026-01-28 16:01:54 -0300

    feat: separete auth routes from auth plugin

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -1,20 +1,17 @@
 import { Elysia } from "elysia";
 import { auth } from "./service";
-import type { AuthContext } from "./model";
 
-export const authController = new Elysia({ name: "auth-controller" })
-  .mount("/auth", auth.handler)
-  .macro({
-    auth: {
-      async resolve({ status, request: { headers } }) {
-        const session = await auth.api.getSession({ headers });
-        if (!session) return status(401);
-        return {
-          user: session.user,
-          session: session.session,
-        };
-      },
+export const authController = new Elysia({ name: "auth-controller" }).mount(
+  "/auth",
+  auth.handler,
+);
+
+export const authPlugin = new Elysia({ name: "auth-plugin" }).macro({
+  auth: {
+    async resolve({ status, request: { headers } }) {
+      const session = await auth.api.getSession({ headers });
+      if (!session) return status(401);
+      return { user: session.user, session: session.session };
     },
-  });
-
-export type { AuthContext };
+  },
+});
