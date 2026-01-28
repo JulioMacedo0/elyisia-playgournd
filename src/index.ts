48b95522d33645b9465c64ccf1372a0e9d20c472
Julio Macedo <juliocesar1076@gmail.com>
2026-01-28 16:01:13 -0300

    feat: config auth and cors

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import { Elysia } from "elysia";
 import { openapi } from "@elysiajs/openapi";
 import { cors } from "@elysiajs/cors";
 import "./lib/env";
-import { authController } from "./modules/auth";
+import { authController, authPlugin } from "./modules/auth";
 import { userController } from "./modules/user";
 import { OpenAPI } from "./modules/auth/service";
+import { env } from "./lib/env";
 
 const app = new Elysia()
   .use(
@@ -18,14 +19,16 @@ const app = new Elysia()
   )
   .use(
     cors({
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      origin:
+        env.NODE_ENV === "development" ? "http://localhost:3000" : env.DOMAIN,
+      methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
       allowedHeaders: ["Content-Type", "Authorization"],
     }),
   )
+  .use(authPlugin)
   .use(authController)
-  .use(userController)
+  .guard({ auth: true }, (app) => app.use(userController))
   .listen(3000);
 
 console.log(
