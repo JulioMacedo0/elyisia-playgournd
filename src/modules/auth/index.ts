974218624c34f8fcdff7dbfbf26f473c38fe263f
Julio Macedo <juliocesar1076@gmail.com>
2026-01-29 11:54:29 -0300

    feat: remove path on elysia controller

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 import { auth } from "./service";
 
 export const authController = new Elysia({ name: "auth-controller" }).mount(
-  "/auth",
+  "",
   auth.handler,
 );
 
