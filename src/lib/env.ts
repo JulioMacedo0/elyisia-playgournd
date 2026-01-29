72159a78301272211faa9e78024c71cdf45dc36f
Julio Macedo <juliocesar1076@gmail.com>
2026-01-29 11:54:11 -0300

    feat: add better auth url

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,15 +3,19 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production"]),
   DATABASE_URL: z.string().url("DATABASE_URL must be a valid URL"),
-  BETTER_AUTH_SECRET: z.string().min(1, "BETTER_AUTH_SECRET is required"),
   DOMAIN: z.url("DOMAIN must be a valid URL").min(1, "DOMAIN is required"),
+  BETTER_AUTH_SECRET: z.string().min(1, "BETTER_AUTH_SECRET is required"),
+  BETTER_AUTH_URL: z
+    .url("BETTER_AUTH_URL must be a valid URL")
+    .min(1, "BETTER_AUTH_URL is required"),
 });
 
 const parsedEnv = envSchema.safeParse({
   NODE_ENV: process.env.NODE_ENV,
   DATABASE_URL: process.env.DATABASE_URL,
-  BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
   DOMAIN: process.env.DOMAIN,
+  BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+  BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
 });
 
 if (!parsedEnv.success) {
